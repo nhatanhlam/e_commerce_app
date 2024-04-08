803c2cba8b1ed5377ab0d38efa33260173a23b3f
nhatanhlam <anh.2174802015019@vanlanguni.vn>
2024-04-08 16:12:08 +0700

    commit_3

---
+++ lib/app/constants/colors_value.dart
@@ -0,0 +1,14 @@
+import 'package:e_commerce_app/app/providers/dark_mode_provider.dart';
+import 'package:e_commerce_app/config/flavor_config.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+FlavorConfig flavor = FlavorConfig.instance;
+
+class ColorsValue {
+  static Color primaryColor(BuildContext context) {
+    return context.read<DarkModeProvider>().isDarkMode
+        ? flavor.flavorValues.roleConfig.primaryDarkColor()
+        : flavor.flavorValues.roleConfig.primaryColor();
+  }
+}
